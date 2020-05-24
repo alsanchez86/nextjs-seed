c7133fdff0439c8ec109a40e0169bf2a3c30065c
asancpiz <asancpiz@everis.com>
2020-05-24 02:04:22 +0200

    add loader

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,10 +1,37 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { setLoading } from "../../contexts/cache/actions";
+import { useCacheContextState, useCacheContextDispatch } from "../../contexts/cache";
 import MainNav from "../main-nav";
 import Footer from "../footer";
+import { ProgressSpinner } from "primereact/progressspinner";
 
-export default ({ children }) =>
+export default ({ children }) => {
+    const router = useRouter();
+    const contextState = useCacheContextState();
+    const contextDispatch = useCacheContextDispatch();
 
-<>
-    <MainNav />
-    {children}
-    <Footer />
-</>
+    useEffect(() => {
+        const handleStart = (url) => ((url !== router.pathname) && contextDispatch(setLoading(true)));
+        router.events.on('routeChangeStart', handleStart);
+        contextDispatch(setLoading(false)); // Load data from cache too
+
+        return () => {
+            router.events.off('routeChangeStart', handleStart);
+        }
+    }, []);
+
+    return (
+        <>
+            <MainNav />
+
+            {contextState?.loadingPage && (
+                <ProgressSpinner strokeWidth={"6"} />
+            )}
+
+            {!contextState?.loadingPage && children}
+
+            <Footer />
+        </>
+    );
+}
